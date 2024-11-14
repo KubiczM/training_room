06b82f20f2aa1c3e0a301a55e696b5b27a5ed54e
Michał Kubicz <mqbicz@gmail.com>
2024-11-14 19:01:58 +0100

    First commit

---
+++ whiteboard.py
@@ -0,0 +1,21 @@
+
+# use subprocess to pull the data by nmap from https://www.forcedeye.com
+import subprocess
+
+def get_SYN_scan(host):
+    result = subprocess.run(['nmap', '-sS', host], capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stdout)
+
+def get_UDP_scan(host):
+    result = subprocess.run(['nmap', '-sU', '-Pn', host], capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
+
+host = "forcedeye.com"
+
+
+
+if __name__ == "__main__":
+    get_SYN_scan(host)
+    get_UDP_scan(host)
